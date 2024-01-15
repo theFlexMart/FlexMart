6984549e7894769239b93cdab13e00ebcbae2646
M A Muquit <mail@muquit.xyz>
2024-01-16 00:02:42 +0600

    fle-11 provision s3 buckets

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 import pulumi
 
 from core.cognito import user_pool, user_pool_client
+from core.s3 import admin_app_bucket, data_bucket
 
 pulumi.export("user-pool-id", user_pool.id)
 pulumi.export("user-pool-client-id", user_pool_client.id)
+pulumi.export("data-bucket", data_bucket.bucket_name)
+pulumi.export("admin-app-bucket", admin_app_bucket.bucket_name)
